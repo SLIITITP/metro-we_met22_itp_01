b385d52c7a3353b417e68292a7c7ace48076b604
Reezan Saleem <it21042324@my.sliit.lk>
2022-10-19 19:34:16 +0530

    view Change

--- Project/frontend/src/components/CustomerService/Transport/Content/showTransportRequest.js
+++ Project/frontend/src/components/CustomerService/Transport/Content/showTransportRequest.js
@@ -116,7 +116,7 @@ export default function ShowTransportRequest() {
         width: "40%",
         overflow: "auto",
         marginTop: "-870px",
-        marginLeft: "900px",
+        marginLeft: "750px",
         position: "fixed",
       }}
     >
